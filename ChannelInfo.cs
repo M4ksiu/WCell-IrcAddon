1eb89dcb31e52a23e19cca8493b5c1356a9f5e96
Jamez <james@jaddie.co.uk>
2010-01-26 04:53:28 +0000

    Updated ChannelInfo thanks alot stupid svn patching. Updated IRCAddon.csproj  -- Fixed output paths.  -- Fixed Stupid client only subset setting

--- ChannelInfo.cs
+++ ChannelInfo.cs
@@ -1,17 +1,4 @@
-&#65279;using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace IRCAddon
-{
-	public class ChannelInfo
-	{
-		public string ChannelName;
-		public string Password;
-	}
-}
-&#65279;using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
