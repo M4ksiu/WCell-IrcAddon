23973ea03ba86108aae6ce999a0c5d464f8fc358
Jamez <james@jaddie.co.uk>
2010-01-30 04:04:18 +0000

    Changed shortname since Domi did not approve.

--- IrcAddon.cs
+++ IrcAddon.cs
@@ -24,7 +24,7 @@ namespace IRCAddon
 
         public override string ShortName
         {
-            get { return "IrcBot"; }
+            get { return "Irc"; }
         }
 
         public override bool UseConfig
